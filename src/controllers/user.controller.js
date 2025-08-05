36ed33428cf8201b06b2b740e9aaeb0a62c0cd38
AjinkyaJoshi05 <aaj.ajinkya@gmail.com>
2025-08-06 00:06:51 +0530

    user controller (register)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,90 @@
 import {asyncHandler}  from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApipError.js";
+import {User} from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req, res)=>{
-    res.status(200).json({
-        mesaage: "Chai aur code..."
+    // get users data from frontend
+
+    // validation -not empty
+
+    // check user alreqady exist (check users and email)
+
+    // check for images and avatar
+
+    // upload them to cloudinary,avatar
+
+    // create a user object - create user entry in db
+
+    // remove password and refresh token from response
+
+    // check for user creation
+
+    // return res
+
+    const {fullName, email, username, password} = req.body;
+    console.log("Email: ",email);
+
+    // if (fullName === ""){
+    //     throw new ApiError(400, "FullName is required!")
+    // } check each using if else
+    
+        // or
+    
+    if (
+
+        [fullName,email,username, password].some((field)=>
+        field ?.trim() === "")
+    ){
+        throw new ApiError(400, "All Fields are required")
+    }
+
+    const existingUser = User.findOne({
+        $or: ({username},{email})
     })
+    if (existingUser){
+        throw new ApiError(409,"User with given username or email exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path; // names from routes
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath){
+        throw new ApiError (400, "Avatar file is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar){
+        throw new ApiError (400, "Avatar file is required");
+    }
+
+    const user = await User.create({
+        fullname: fullName,
+        avatar:avatar.url,
+        coverImage: coverImage?.url,
+        email,
+        password,
+        username: username.toLowerCase(),
+
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"  // write all the fields you donot want to select
+    );
+
+    if (!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse (200, createdUser, "User registered Successfully")
+    )
+
+
 })
 
 
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
-
-
-
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar", // this name has to be same in frontend
+            maxCount:1 
+        },
+        {
+            name: "coverImage",
+            maxCount:1
+        }
+    ]),
+    // this is the way to inject a moddleware.
+    // call one just before the function.
+    registerUser);
 export default router;
